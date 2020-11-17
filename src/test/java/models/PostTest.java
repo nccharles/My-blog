8c0769d6179d2dd90a081e2dd607e40497d3d62d
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-17 22:52:47 +0200

    some tests

---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,20 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+
+    }
+}
